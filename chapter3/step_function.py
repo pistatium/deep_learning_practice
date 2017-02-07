343c8484f4087206bebefb4ab3d02a87bedc437c
kimihiro_n <kimihiro_n@pistatium.dip.jp>
2017-02-07 12:33:42 +0900

    Create step_function

---
+++ chapter3/step_function.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def main():
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = step_function(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
