fb54b5bc8778515a6f1c47dd22e7e78087f440c5
kimihiro_n <kimihiro_n@pistatium.dip.jp>
2017-02-07 12:21:33 +0900

    Fixed xor

--- chapter2/xor.py
+++ chapter2/xor.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-from .and_node import AND
-from .or_node import OR
-from .nand_node import NAND
+from and_node import AND
+from or_node import OR
+from nand_node import NAND
 
 
 def XOR(x1, x2):
@@ -11,9 +11,9 @@ def XOR(x1, x2):
 
 def main():
     assert XOR(0, 0) == 0
-    assert XOR(1, 0) == 0
-    assert XOR(0, 1) == 0
-    assert XOR(1, 1) == 1
+    assert XOR(1, 0) == 1
+    assert XOR(0, 1) == 1
+    assert XOR(1, 1) == 0
 
 if __name__ == '__main__':
     main()
